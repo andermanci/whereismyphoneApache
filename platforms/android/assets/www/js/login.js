70b47261cf23e88c46e4902597dee6c47a7d8f14
UrtziOtamendi <urtziotamendi@gmail.com>
2017-04-24 15:14:53 +0200

    Bigarren igoera
    Lehen saiakera login interfazea egiten; interfazea eginaa eta datu base lokalarekin konektatua, baina herokukoa probatu gabe

--- platforms/android/assets/www/js/login.js
+++ platforms/android/assets/www/js/login.js
@@ -2,32 +2,34 @@
  * Created by andermanci on 21/4/17.
  */
 
-function init(){
-    documen.addEventListener("deviceready", deviceready, true);
-    delete init;
-}
+
 
 
 function handleLogin() {
+    console.log("1");
     var form = $("#loginForm");
     //disable the button so we can't resubmit while we wait
-    $("#submitButton",form).attr("disabled","disabled");
+    $("#login",form).attr("disabled","disabled");
     var user = $("#username", form).val();
     var password = $("#password", form).val();
     if(user != '' && password!= '') {
-        $.post("http://www.coldfusionjedi.com/demos/2011/nov/10/service.cfc?method=login&returnformat=json", {username:u,password:p}, function(res) {
-            if(res == true) {
-                //store
-                window.localStorage["username"] = user;
-                window.localStorage["password"] = password;
-                $.mobile.changePage("some.html");
-            } else {
-                navigator.notification.alert("Your login failed", function() {});
-            }
-            $("#submitButton").removeAttr("disabled");
-        },"json");
+        var SHA1 = require("crypto-js/sha1");
+        var pg= require('pg');
+        var connectionURI="    postgres://ofwsfkgedzztju:fbd2eab314bba2b3c81cd727730947c9ef5445f01bb2542821a124b2c99b4f98@ec2-79-125-125-97.eu-west-1.compute.amazonaws.com:5432/d32n963hbr8ngb";
+        var database= new pg.client(connectionURI);
+        database.connect();
+        var password_crypted=SHA1(password);
+        var query= database.query("SELECT id from User where username="+user+"and password="+password_crypted);
+        if(query._size > 0){
+            window.localStorage["username"]=user;
+            window.localStorage["password"]=password_crypted;
+            navigator.notification.alert("Osondo", function() {});
+        } else {
+            navigator.notification.alert("Your login failed", function() {});
+        }
+        $("#submitButton").removeAttr("disabled");
+
     } else {
-        //Thanks Igor!
         navigator.notification.alert("You must enter a username and password", function() {});
         $("#submitButton").removeAttr("disabled");
     }
